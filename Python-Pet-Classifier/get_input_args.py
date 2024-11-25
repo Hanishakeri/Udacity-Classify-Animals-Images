1b43ad606a1ed90c86f2db82aca967cc97d7f076
Hanieh Shakeri <115367459+Hanishakeri@users.noreply.github.com>
2024-11-25 20:42:16 +0100

    Update get_input_args.py

--- Python-Pet-Classifier/get_input_args.py
+++ Python-Pet-Classifier/get_input_args.py
@@ -2,9 +2,9 @@
 # -*- coding: utf-8 -*-
 # */AIPND-revision/intropyproject-classify-pet-images/get_input_args.py
 #                                                                             
-# PROGRAMMER: 
-# DATE CREATED:                                   
-# REVISED DATE: 
+# PROGRAMMER: Hanieh Shakeri
+# DATE CREATED: 15.Nov.2024                                   
+# REVISED DATE: 24.Nov.2024
 # PURPOSE: Create a function that retrieves the following 3 command line inputs 
 #          from the user using the Argparse Python module. If the user fails to 
 #          provide some or all of the 3 inputs, then the default values are
